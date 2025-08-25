c818b07283c591645170ac67babd24713a6e1347
First <pyimcha@cmkl.ac.th>
2025-08-25 10:03:56 +0700

    Add generate masks file

---
+++ generate_masks.py
@@ -0,0 +1,28 @@
+import json
+import numpy as np
+import cv2
+from pathlib import Path
+from PIL import Image
+
+input_dir = Path("dataset_train")   # folder with .png and .json
+output_mask_dir = Path("dataset_train/masks")  
+output_mask_dir.mkdir(parents=True, exist_ok=True)
+
+for json_file in input_dir.glob("*.json"):
+    with open(json_file, "r") as f:
+        data = json.load(f)
+
+    h, w = data["imageHeight"], data["imageWidth"]
+    mask = np.zeros((h, w), dtype=np.uint8)
+
+    for shape in data["shapes"]:
+        if shape["shape_type"] == "polygon":
+            pts = np.array(shape["points"], dtype=np.int32)
+            pts = pts.reshape((-1, 1, 2))
+            cv2.fillPoly(mask, [pts], 1)  # fill with 1s
+
+    # Save mask as PNG (0 background, 255 nerve)
+    mask_path = output_mask_dir / (json_file.stem + "_mask.png")
+    Image.fromarray(mask*255).save(mask_path)
+
+print("Mask generation complete!")
